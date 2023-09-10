ccc980e0613e532684eb25a14c58220a1633ec17
Air <samuelnegalign1@gmail.com>
2023-09-10 11:25:49 +0300

    adding comments and conditional rendering

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import React,{useState,useEffect} from "react";
 
 
 function App() {
+
+  // States to store different variables
+  
    const [title,setTitle] = useState('Design Liberaized design state management')
    const [img,setImg] = useState('https://images.pexels.com/photos/1921326/pexels-photo-1921326.jpeg?auto=compress&cs=tinysrgb&w=600')
    const [author,setAuthor] = useState('Samuel Negalign')
    const [summary,setSummary] = useState('This is a2sv test react app. It has card compenents')
    const [date,setDate] = useState('Jan 10 2023')
 
+  //  I used tailwind css for styling
   return (
     <div class = 'h-screen p-4 mx-4 my-6  shadow '>
       <h1 class = 'ml-5 my-4 font-bold'>Related Blogs</h1>
