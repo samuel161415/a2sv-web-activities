087cfc3ba4908bef4f3a52d19dc79acf5d236050
Air <samuelnegalign1@gmail.com>
2023-09-14 21:56:58 +0300

    adding like buttons and comments

--- src/App.js
+++ src/App.js
@@ -7,19 +7,29 @@ function App() {
 
   // States to store different variables
   
-   const [title,setTitle] = useState('Design Liberaized design state management')
-   const [img,setImg] = useState('https://images.pexels.com/photos/1921326/pexels-photo-1921326.jpeg?auto=compress&cs=tinysrgb&w=600')
-   const [author,setAuthor] = useState('Samuel Negalign')
-   const [summary,setSummary] = useState('This is a2sv test react app. It has card compenents')
-   const [date,setDate] = useState('Jan 10 2023')
+    const [title,setTitle] = useState('Design Liberaized design state management')
+    const [img,setImg] = useState('https://images.pexels.com/photos/1921326/pexels-photo-1921326.jpeg?auto=compress&cs=tinysrgb&w=600')
+    const [author,setAuthor] = useState('Samuel Negalign')
+    const [summary,setSummary] = useState('This is a2sv test react app. It has card compenents')
+    const [date,setDate] = useState('Jan 10 2023')
+
+    // this are used to handle number of likes 
+    const  [likes1, setLikes1] = useState(0)
+    const  [likes2, setLikes2] = useState(0)
+
+    // this are used to handle comments of each blog posts
+    const [comment1, setComment1] = useState([])
+    const [comment2, setComment2] = useState([])
+
+
 
   //  I used tailwind css for styling
   return (
-    <div class = 'h-screen p-4 mx-4 my-6  shadow '>
+    <div class = 'h-screen p-4 mx-4 my-6  shadow overflow-auto'>
       <h1 class = 'ml-5 my-4 font-bold'>Related Blogs</h1>
       <div class = 'flex justify-between'>
-        <Card title={title} img={img} author={author} summary = {summary } date = {date}/>
-        <Card title={title} img={img} author={author} summary = {summary } date = {date}/>
+        <Card title={title} img={img} author={author} summary = {summary } date = {date} likes = {likes1} setLikes = {setLikes1} comment = {comment1} setComment = {setComment1}/>
+        <Card title={title} img={img} author={author} summary = {summary } date = {date} likes = {likes2} setLikes = {setLikes2} comment = {comment2} setComment = {setComment2}/>
       </div>
       
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Margin } from '@mui/icons-material';
+import SendIcon from '@mui/icons-material/Send';
 
-function Card({title,img, author , summary,date}) {
+// Card component
+
+
+function Card({title,img, author , summary,date, likes, setLikes, comment, setComment}) {
+    
+    // to set the visiblity of comments
+    const [visible, setVisible] = useState(false)
+
+    // to hold temporary comments
+    const [c_comment,setCurrentComment] = useState('')
+
+    // This two methods are used to handle pushing new commnets to existing comment array.
+    // first one is using button second one handles enter action key
+    const handleComment = () => {
+        c_comment && setComment([...comment, c_comment]);
+        setCurrentComment('')
+      };
+
+    const handleEnter = (e) =>{
+       if(e.key === 'Enter' && c_comment) {
+        setComment([...comment, c_comment]);
+        setCurrentComment('')
+
+       }
+    }
+    
+
   return (
-    <div class = 'flex flex-col shadow-lg rounded h-4/5 mx-6 my-4 w-1/2 text-gray-500 m-3'>
+    <div class = 'flex flex-col shadow-lg rounded h-4/5 mx-6 my-4 w-1/2 text-gray-500 m-3 overflow-auto'>
        {img && <img src={img} class = ' rounded-t-lg h-[350px]'/>} 
         <div class = 'flex flex-col p-4 '>
             {title && <h1 class = 'font-bold h-1/2 m-2'>{title}</h1>}
@@ -18,8 +45,17 @@ function Card({title,img, author , summary,date}) {
             {summary && <p class = 'm-2'>{summary}</p>}
             <section class = 'flex justify-between m-2 border-t-2 my-3 py-3'>
                 
-                <span > <ChatBubbleIcon style = {{height: '30px', width : '30px'}}/> 2.3k likes</span> 
-                <span class = 'text-blue-700'>read more</span>
+                <span onClick={()=>setLikes(prev=>prev+1)}> <ChatBubbleIcon style = {{height: '30px', width : '30px'}} /> {likes} likes</span> 
+                <span class = 'text-blue-700' onClick={() => setVisible(!visible)}>Add Comment</span>
+            </section>
+            <section class = {`${visible ? 'visible' :'invisible'} flex justify-end  '`}>
+                 
+                <div>{comment.map(comment =><p class = 'p-2 italic'>{comment}</p>)}</div>
+            </section>
+            <section class = {`${visible ? 'visible' :'invisible'} flex justify-end  '`}>
+                < AccountCircleIcon style={{height :'40px',width : '40px'}}/> 
+                <input value={c_comment} type="text" placeholder='Add a comment...' class = 'p-2 mx-2 border-2 rounded-lg' onChange={(e)=>setCurrentComment(e.target.value)} onKeyDown={handleEnter}/>
+                <span onClick={handleComment}>< SendIcon style={{height :'40px',width : '40px', color : 'blue'}}/></span>
             </section>
         </div>
     </div>
